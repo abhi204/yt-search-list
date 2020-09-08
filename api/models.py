3513a660054ac0e8ec18aadc32890e5c3706566a
abhi204 <abhi.chaurasia204@gmail.com>
2020-09-09 03:23:13 +0530

    Create Video model

--- api/models.py
+++ api/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Video(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    publish_datetime = models.DateTimeField()
+    thumbnail_url = models.TextField()
